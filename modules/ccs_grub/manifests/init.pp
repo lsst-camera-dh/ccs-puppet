c397fb04b551af6665399d758e8a4cd7ddb46e01
Glenn Morris <gmorris@slac.stanford.edu>
2020-04-14 17:21:08 -0700

    Tweak heredoc to avoid puppet-lint parse error

--- modules/ccs_grub/manifests/init.pp
+++ modules/ccs_grub/manifests/init.pp
@@ -5,8 +5,8 @@ class ccs_grub {
     path    => [ '/usr/bin' ],
     unless  => 'grep -q usbhid.quirks /etc/default/grub',
     command => @(CMD/L),
-      sed -i '/^GRUB_CMDLINE_LINUX=/ s/"$/ \
-      usbhid.quirks=0x413c:0x301a:0x00000400,0x04ca:0x0061:0x00000400"/' \
+      sed -i "/^GRUB_CMDLINE_LINUX=/ s/\"$/ \
+      usbhid.quirks=0x413c:0x301a:0x00000400,0x04ca:0x0061:0x00000400\"/" \
       /etc/default/grub
       | CMD
     notify  => Exec['grub2-mkconfig'],
