7ec0d90d43c1d77e830337366192e116deaad4e4
Glenn Morris <gmorris@slac.stanford.edu>
2020-04-16 11:25:44 -0700

    Restore deleted executable fact
    ccs_facts/mounted.sh is still needed for inodes.
    Restore, and move to the mrtg module, which is the only user.

---
+++ modules/ccs_mrtg/facts.d/ccs_mrtg-inodes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## Facter mountpoints.m.size_bytes tells us the size in bytes,
+## but not the inodes.
+for m in / /data /home /opt /scratch /tmp /var ; do
+
+    if [ "$m" == "/" ]; then
+        mount=root
+    else
+        mount=${m#/}
+        mount=${mount//\//_}
+    fi
+
+    [ -d "$m" ] && grep -q "$m " /etc/mtab || continue
+
+    inodes=$(df --output=itotal $m | tail -n 1)
+    printf "inodes_${mount}=$inodes\n"
+
+done
+
+
+exit 0
