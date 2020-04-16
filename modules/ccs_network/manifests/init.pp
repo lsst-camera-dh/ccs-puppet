1699a8142eeff11e52572568aad4b24780a17e7d
Glenn Morris <gmorris@slac.stanford.edu>
2020-04-16 12:28:09 -0700

    Replace daq_networking fact with standard networking.interfaces one

--- modules/ccs_network/manifests/init.pp
+++ modules/ccs_network/manifests/init.pp
@@ -2,14 +2,32 @@
 ##   Add settings for network interfaces.
 ##
 ## @param daq_interface
-##   String naming the DAQ network interface (empty if none).
+##   String naming the DAQ network interface; or true to detect it; or
+##   false if none.
 
-class ccs_network (String $daq_interface = '') {
+class ccs_network (Variant[Boolean,String] $daq_interface = false) {
 
-  unless empty($daq_interface) {
+  if $daq_interface {
+
+    ## Normally the interface is "lsst-daq", but not always (eg lsst-dc02).
+    ## TODO discover it?
+    ## Could check for an interface connected to 192.168.100.1?
+    $idefault = 'p3p1'
+    $imaybe = 'lsst-daq'
+
+    ## lsst-daq if exists, else p3p1.
+    $interface0 = $facts['networking']['interfaces'][$imaybe] ? {
+      undef   => $idefault,
+      default => $imaybe,
+    }
+
+    $interface1 = $daq_interface ? {
+      String  => $daq_interface,
+      default => $interface0 ,
+    }
 
     ## Note: asked not to modify DAQ network interfaces.
-    $interface = "DISABLED-${daq_interface}"
+    $interface = "DISABLED-${interface1}"
 
     $file = '30-ethtool'
 
