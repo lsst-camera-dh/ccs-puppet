54af40344846260748205ac63f32031c490f8cd5
Glenn Morris <gmorris@slac.stanford.edu>
2020-05-07 16:04:27 -0700

    Update jdk8 version
    Also add a workaround in cases alternatives get corrupted

--- site-modules/ccs_software/manifests/jdk8.pp
+++ site-modules/ccs_software/manifests/jdk8.pp
@@ -2,12 +2,14 @@ class ccs_software::jdk8 {
 
   $ccs_pkgarchive = lookup('ccs_pkgarchive', String)
 
-  ## TODO hiera
-  $jdkrpm = 'jdk-8u112-linux-x64.rpm'
-  ## FIXME: rpm -qi -p ${jdkrpm} | gawk '/^Version/ {print $3}'
-  $javaver = '1.8.0_112'
-  ## FIXME: rpm -q -p $jdkrpm
-  $javapkg = 'jdk1.8.0_112-1.8.0_112-fcs.x86_64'
+  ## FIXME hiera
+  $jdkrpm = 'jdk-8u251-linux-x64.rpm'
+  ## The name of the package installed by the rpm.
+  ## rpm -q -p $jdkrpm
+  $javapkg = 'jdk1.8-1.8.0_251-fcs.x86_64'
+  ## The rpm installs to /usr/java/jdk${javaver}
+  ## Almost: rpm -qi -p ${jdkrpm} | awk '/^Version/ {print $3}'
+  $javaver = '1.8.0_251-amd64'
 
   $jdkfile = "/var/tmp/${jdkrpm}"
 
@@ -20,11 +22,16 @@ class ccs_software::jdk8 {
   exec { 'Install jdk8':
     path      => ['/usr/bin'],
     unless    => "rpm -q --quiet ${javapkg}",
-    command   => "rpm -i ${jdkfile}",
+    ## Note that (some?) jdk8 rpms have the version in the name,
+    ## eg "jdk1.8.0_112" rather than "jdk1.8", so that even with -U
+    ## one ends up with multiple copies installed.
+    ## https://bugs.openjdk.java.net/browse/JDK-8055864
+    command   => "rpm -U ${jdkfile}",
     subscribe => Archive[$jdkfile],
   }
 
 
+  ## TODO https://forge.puppet.com/puppet/alternatives  ?
   $cmds = ['java', 'javac', 'javaws', 'jar', 'jconsole', 'jstack']
 
   $cmds.each |$cmd| {
@@ -32,8 +39,19 @@ class ccs_software::jdk8 {
     $dest = "/usr/java/jdk${javaver}/bin/${cmd}"
     exec {"java alternative for ${cmd}":
       path    => ['/usr/sbin', '/usr/bin'],
-      command => "sh -c \"update-alternatives --install ${src} ${cmd} ${dest} 1000 && update-alternatives --set ${cmd} ${dest}\"",
-      unless  => "sh -c \"update-alternatives --display ${cmd} | grep -q 'currently points to '${dest}\""
+      ## The alternatives system seems to be flimsy.
+      ## It can get into a confused state where you have to fix it
+      ## with things like: rm /var/lib/alternatives/javaws
+      command => @("CMD"/L),
+        sh -c "test -e /etc/alternatives/${cmd} || \
+        rm -f /var/lib/alternative/${cmd}; \
+        update-alternatives --install ${src} ${cmd} ${dest} 1000 && \
+        update-alternatives --set ${cmd} ${dest}"
+        | CMD
+      unless  => @("CMD"/L),
+        sh -c "update-alternatives --display ${cmd} | \
+        grep -q 'currently points to ${dest}'"
+        | CMD
     }
   }
 
